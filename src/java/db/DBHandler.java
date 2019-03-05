ae8afb25d84dfe4865d692d767bfcc3feacec92f
Artem Popov <aptem336@gmail.com>
2019-03-06 06:43:41 +0800

    Trying to add dynamic

--- src/java/db/DBHandler.java
+++ src/java/db/DBHandler.java
@@ -1,67 +1,40 @@
 package db;
 
-import entities.*;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.util.HashMap;
-import java.util.ArrayList;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 public class DBHandler {
 
-    public final static HashMap<String, ActiveRecord> UNITS = new HashMap<>();
-    public final static HashMap<String, ArrayList<ActiveRecord>> DATA = new HashMap<>();
+    public final static HashMap<String, DAO> TABLES = new HashMap<>();
 
     static {
-        UNITS.put(new Address().getTableName(), new Address());
-        UNITS.put(new Sector().getTableName(), new Sector());
-        UNITS.put(new Status().getTableName(), new Status());
-        UNITS.put(new Purpose().getTableName(), new Purpose());
-        UNITS.put(new Doctor().getTableName(), new Doctor());
-        UNITS.put(new Diagnosis().getTableName(), new Diagnosis());
-        UNITS.put(new Patient().getTableName(), new Patient());
-        UNITS.put(new Visit().getTableName(), new Visit());
-        UNITS.values().forEach((record) -> {
-            readData(record);
-        });
-    }
-
-    private static void readData(ActiveRecord record) {
-        try {
-            ArrayList<ActiveRecord> records = new ArrayList<>();
-            ResultSet data = getResultSet("SELECT * FROM \"" + record.getTableName() + "\"");
-            while (data.next()) {
-                Object[] row = new Object[data.getMetaData().getColumnCount()];
-                for (int i = 0; i < row.length; i++) {
-                    row[i] = data.getObject(i + 1);
+        try (Connection connection = getConnection(); ResultSet table_names = connection.getMetaData().getTables("postgres", "public", "%", new String[]{"TABLE"})) {
+            while (table_names.next()) {
+                String table_name = table_names.getString("TABLE_NAME");
+                ResultSetMetaData rsmd = connection.createStatement().executeQuery("SELECT * FROM \"" + table_name + "\"").getMetaData();
+                HashMap<String, String> column = new HashMap<>();
+                for (int i = 0; i < rsmd.getColumnCount(); i++) {
+                    column.put(rsmd.getColumnName(i + 1), rsmd.getColumnTypeName(i + 1));
                 }
-                records.add(record.cast(row));
+                TABLES.put(table_name, new DAO(table_name, column));
             }
-            DATA.put(record.getTableName(), records);
-        } catch (SQLException ex) {
-            //TO-DO
-            System.out.println(ex.getMessage());
-        }
-    }
-
-    public static ResultSet getResultSet(String select_query) {
-        try {
-            return getConnection().createStatement().executeQuery(select_query);
         } catch (NamingException | SQLException ex) {
-            //TO-DO
             System.out.println(ex.getMessage());
-            return null;
         }
     }
 
-    public static void executeUpdate(String update_query) throws NamingException, SQLException {
-        getConnection().createStatement().executeUpdate(update_query);
+    public static PreparedStatement getPreparedStatement(String query) throws NamingException, SQLException {
+        return getConnection().prepareStatement(query);
     }
 
-    private static Connection getConnection() throws NamingException, SQLException {
+    public static Connection getConnection() throws NamingException, SQLException {
         InitialContext initContext = new InitialContext();
         DataSource dataSource = (DataSource) initContext.lookup("java:comp/env/jdbc/sql");
         return dataSource.getConnection();
--- src/java/db/Servlet.java
+++ src/java/db/Servlet.java
@@ -2,16 +2,14 @@ package db;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.SQLException;
 import java.util.HashMap;
-import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class Servlet extends HttpServlet {
-
+    
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
@@ -26,30 +24,28 @@ public class Servlet extends HttpServlet {
             String[] type = parameters.get("type");
             if (type != null) {
                 String table_name = parameters.get("table_name")[0];
-                out.println("<span class=\"table_title\">" + table_name + "</span>");
                 out.println("<script type=\"text/javascript\">");
                 out.println("\tparent.document.getElementById('table_name').setAttribute('value', '" + table_name + "');");
                 out.println("</script>");
-                out.println(type[0]);
-                try {
-                    switch (type[0]) {
-                        case "delete":
-                            DBHandler.DATA.get(table_name).get(Integer.parseInt(parameters.get("row_index")[0])).delete();
-                            break;
-                    }
-                    out.print(HTMLBuilder.buildHTMLTable(table_name));
-                    out.println("<script type=\"text/javascript\" src=\"DB/script.js\"></script>");
-                } catch (NamingException | SQLException ex) {
-                    out.println(ex.getMessage());
+                out.println("<span class=\"table_title\">" + table_name + "</span><br>");
+                out.println(type[0] + "<br>");
+                switch (type[0]) {
+                    case "insert":
+                        DBHandler.TABLES.get(table_name).column.keySet().forEach((name) -> {
+                            out.println(name + "<br>");
+                            out.println("<input type=\"" + TypeMapping.MAPPING.get(DBHandler.TABLES.get(table_name).column.get(name)).HTML_TYPE + "\"><br>");
+                        });
+                        break;
                 }
+                out.println("<script type=\"text/javascript\" src=\"DB/script.js\"></script>");
             } else {
                 out.println("\t<form action=\"\" target=\"data_frame\" method=\"post\">");
-                DBHandler.UNITS.values().forEach((record) -> {
-                    out.println("\t\t<button name=\"table_name\" value=\"" + record.getTableName() + "\" type=\"submit\">" + record.getTableName() + "</button>");
+                DBHandler.TABLES.keySet().forEach((record) -> {
+                    out.println("\t\t<button name=\"table_name\" value=\"" + record + "\" type=\"submit\">" + record + "</button>");
                 });
                 out.println("\t\t<input type=\"hidden\" name=\"type\" value=\"\">");
                 out.println("\t</form>");
-
+                
                 out.println("\t<form action=\"\" target=\"data_frame\" method=\"post\">");
                 out.println("\t\t<button name=\"type\" value=\"insert\" type=\"submit\">Добавить</button>");
                 out.println("\t\t<button name=\"type\" value=\"update\" type=\"submit\">Изменить</button>");
@@ -57,7 +53,7 @@ public class Servlet extends HttpServlet {
                 out.println("\t\t<input id=\"table_name\" type=\"hidden\" name=\"table_name\">");
                 out.println("\t\t<input id=\"row_index\" type=\"hidden\" name=\"row_index\">");
                 out.println("\t</form>");
-
+                
                 out.println("\t<iframe name=\"data_frame\" src=\"?type=\"\"></iframe>");
             }
             out.println("</body>");
@@ -71,7 +67,7 @@ public class Servlet extends HttpServlet {
             throws ServletException, IOException {
         processRequest(request, response);
     }
-
+    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
